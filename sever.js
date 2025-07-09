80b7631d2bb48d444aef076a4f07d63a9e33f07f
compiter <kynyay5678@gmail.com>
2025-07-09 15:24:53 +0530

    updated

---
+++ sever.js
@@ -0,0 +1,40 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/userRoute.js";
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(express.static("public"));
+const dbuser = encodeURIComponent(process.env.DBUSER);
+const dbpass = encodeURIComponent(process.env.DBPASS);
+
+// mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
+//   app.listen(8080, () => {
+//     console.log("Server started");
+//   });
+// });
+
+// mongoose
+// .connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.tiuin8d.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
+
+// )
+// .then(() => {
+//     app.listen(8080, () => {
+//     console.log("Server started");
+//     });
+// });
+
+// app.use("/api/users", userRouter);
+
+
+mongoose
+.connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.tiuin8d.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`)
+.then(() => {
+app.listen(8080, () => {
+console.log("✅ Server started");
+});
+})
+.catch((err) => {
+console.error("❌ MongoDB connection error:", err.message);
+});
