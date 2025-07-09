65bfeb9907280d4151765aa95d9acd7a21f270d2
compiter <kynyay5678@gmail.com>
2025-07-09 16:13:13 +0530

    updated

---
+++ server.js
@@ -0,0 +1,78 @@
+// import express from "express";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import userRouter from "./routes/userRoute.js";
+// dotenv.config();
+// const app = express();
+// app.use(express.json());
+// app.use(express.static("public"));
+// const dbuser = encodeURIComponent(process.env.DBUSER);
+// const dbpass = encodeURIComponent(process.env.DBPASS);
+
+// // mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
+// //   app.listen(8080, () => {
+// //     console.log("Server started");
+// //   });
+// // });
+
+// // mongoose
+// // .connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.tiuin8d.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
+
+// // )
+// // .then(() => {
+// //     app.listen(8080, () => {
+// //     console.log("Server started");
+// //     });
+// // });
+
+// // app.use("/api/users", userRouter);
+
+
+// mongoose
+// .connect(`mongodb+srv://${dbuser}:${dbpass}@cluster0.tiuin8d.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`)
+// .then(() => {
+// app.listen(8080, () => {
+// console.log("✅ Server started");
+// });
+// })
+// .catch((err) => {
+// console.error(" MongoDB connection error:", err.message);
+// });
+
+// app.use("/api/users", userRouter);
+
+
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/userRoute.js";
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.static("public"));
+
+// Read and encode DB credentials
+const dbuser = encodeURIComponent(process.env.DBUSER);
+const dbpass = encodeURIComponent(process.env.DBPASS);
+
+// Connect to MongoDB Atlas
+mongoose
+.connect(
+    `mongodb+srv://${dbuser}:${dbpass}@cluster0.tiuin8d.mongodb.net/merncafe?retryWrites=true&w=majority&appName=Cluster0`
+)
+.then(() => {
+    app.listen(8080, () => {
+    console.log("Server started on http://localhost:8080");
+    });
+})
+.catch((err) => {
+    console.error(" MongoDB connection error:", err.message);
+});
+
+// Routes
+app.use("/api/users", userRouter);
